1b943c33a13e0705338c39872baef8e54e831804
James Halim <tirtajames45@gmail.com>
2023-01-14 23:41:33 +0700

    newest

--- functions.h
+++ functions.h
@@ -1,22 +1,25 @@
-static float ifNotZero(float var, float total){
-	if (var != 0){
-		return total * var;
+static int countTilNull(char *argv[], int currArgv){
+	int i = 0;
+	while (argv[currArgv][i] != '\0'){
+		++i;
 	}
-	return total;
+	return i;
 }
 
-static int strInStr(int substrLen, const char substr[substrLen], char *argv[], int currArgv){
-	if (argv[currArgv][0] != substr[0]){
+static int inStr(int substrLen, char substr[], char *argv[], int currArgv, int argvLen)
+{
+	if (argv[currArgv][0] != substr[0]) {
 		return 0;
 	}
-	int i=0;
-	while (argv[currArgv][i] != 0){
-		if (argv[currArgv][i] == substr[i]){
-			if (i == 1){
+
+	int substrEnd = substrLen - 1;
+
+	for (int i = 0; i <= argvLen; ++i) {
+		if (argv[currArgv][i] == substr[i]) {
+			if (i == substrEnd) {
 				return 1;
 			}
 		}
-		++i;
 	}
 	return 0;
 }
